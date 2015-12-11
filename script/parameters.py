703651656f947413e3a0f6c9aedee4ec803e2b88
Radu Popescu <radu@radupopescu.net>
2015-12-11 01:11:48 +0100

    Cleaning up parameter reading
    * Putting parameter reading into separate module
    * Cover both reading command line arguments and JSON file

---
+++ script/parameters.py
@@ -0,0 +1,49 @@
+import argparse
+import json
+
+def read():
+    parser = argparse.ArgumentParser(description='Generate a new C++ project')
+
+    parser.add_argument('project_name', nargs="?",
+                        help='the name of the new project; must be a single word (default: "NewProject")')
+    parser.add_argument('-d', '--description', help='brief description of the project')
+    parser.add_argument('-n', '--developer_name', help='the name of the project developer')
+    parser.add_argument('-g', '--github', help='name of the associated GitHub repository')
+    parser.add_argument('-o', '--output_path',
+                        help='output path where the project will be created (default: "./")')
+    parser.add_argument('-t', '--template_file',
+                        help='JSON file that contains the parameters of the new project. '
+                        + 'If this argument is provided, the others are not considered.')
+
+    arguments = parser.parse_args()
+
+    template_file = arguments.template_file
+    if template_file != None:
+        parameters = get_parameters_from_file(template_file)
+    else:
+        parameters = get_parameters_from_arguments(arguments)
+
+    # Default parameter values, where needed. We do it here s.t. we don't don't have to do it twice,
+    # once for the parser.add_argument call and one more time in get_parameters_from_file
+    if parameters['project_name'] == None:
+        parameters['project_name'] = 'NewProject'
+    if parameters['output_path'] == None:
+        parameters['output_path'] = './'
+
+    return parameters
+
+def get_parameters_from_file(template_file):
+    with open(template_file) as tf:
+        parameters =json.load(tf)
+
+        return parameters
+
+def get_parameters_from_arguments(arguments):
+    params = {'project_name'   : arguments.project_name,
+              'description'    : arguments.description,
+              'developer_name' : arguments.developer_name,
+              'github'         : arguments.github,
+              'output_path'    : arguments.output_path}
+
+    return params
+
