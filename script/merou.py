8b0c617327c1d84325707804d5b78013e8992cff
Radu Popescu <mail@radupopescu.net>
2015-12-12 22:32:45 +0100

    Adding Python module template.py
    This module contains the functions that configure the new project.

--- script/merou.py
+++ script/merou.py
@@ -5,14 +5,23 @@ def main():
     # Collect the project parameters either from command-line arguments or from JSON file
     project_params = parameters.read()
 
-    print "Creating new project:"
-    print "Project name        : ", project_params['project_name']
-    print "Project description : ", project_params['description']
-    print "Developer name      : ", project_params['developer_name']
-    print "GitHub repo         : ", project_params['github']
-    print "Output path         : ", project_params['output_path']
+    print
+    print 'New project:'
+    print 'Project name        : ', project_params['project_name']
+    print 'Project description : ', project_params['description']
+    print 'Developer name      : ', project_params['developer_name']
+    print 'GitHub repo         : ', project_params['github']
+    print 'Output path         : ', project_params['output_path']
+    print
 
+    # Creating new project files
+    print 'Creating new project... ',
     fileoperations.new_project_tree(project_params)
+    print 'done.'
+
+    # Configure the new project replacing the template values
+    print 'Configuring new project... ',
+
 
 if __name__ == '__main__':
     main()
---
+++ script/template.py
@@ -0,0 +1,26 @@
+import datetime
+import re
+
+def build_substitution_table(params):
+    table = { '<PROJECT_NAME>' : params['project_name'],
+              '<PROJECT_NAME_LOWER>' : params['project_name'].lower(),
+              '<CURRENT_YEAR>' : datetime.datetime.now.year,
+              '<OWNER_NAME>' : params['owner_name'],
+              '<REPO_NAME>' : params['github'],
+              '<PROJECT_OVERVIEW>' : params['description'] }
+
+    return table
+
+def substitute_in_string(str, substitutions):
+    str_buf = str
+    for pattern, value in substitutions:
+        str_buf = re.sub(pattern, value, str_buf)
+
+    return str_buf
+
+def substitute_in_file(file, substitutions):
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    with open(file_name, 'w') as f:
+        for line in lines:
+            sources.write(substitute_in_string(line, substitutions))
