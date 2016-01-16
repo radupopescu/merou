2bbb385bd8626001f075dea4225c57aeeebb4c26
Radu Popescu <mail@radupopescu.net>
2016-01-16 21:48:10 +0100

    Adding license headers

--- project_template/tests/test_main.cpp
+++ project_template/tests/test_main.cpp
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file is part of the <PROJECT_NAME> project:
+ *     https://github.com/<GITHUB_USER_NAME>/<GITHUB_REPO_NAME>
+ */
+
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
